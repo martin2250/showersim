cf9906322e997c15a4aa21d0009cfb3e28ed5704
Martin Pittermann <martinp2250@gmail.com>
2020-12-18 11:16:00 +0100

    switch to counting generations and improve distribution

--- plot.py
+++ plot.py
@@ -2,13 +2,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-depth, particles, pions, electrons, photons, muons, neutrinos = np.loadtxt('result.txt', unpack=True)
+depth, particles, pions, electrons, photons, muons, neutrinos, energy = np.loadtxt('result.txt', unpack=True)
 
 plt.plot(depth, pions, label='pions')
 plt.plot(depth, muons, label='muons')
 plt.plot(depth, electrons, label='electrons')
 plt.plot(depth, photons, label='photons')
 plt.plot(depth, neutrinos, label='neutrinos')
+plt.plot(depth, energy / 1e9, label='energy (GeV)')
 
 plt.yscale('log')
 plt.legend()
--- showersim.cpp
+++ showersim.cpp
@@ -22,19 +22,18 @@ struct Simulation {
     double *photons;
     double *muons;
     double *neutrinos;
+    eV_t   *energy;
 
     std::mt19937 generator;
 
     meters_t c_us;
 
     size_t particles_total;
-    size_t particles_active;
 
     Simulation (size_t n_points, meters_t depth_step) :
         n_points(n_points),
         depth_step(depth_step),
-        particles_total(0),
-        particles_active(0)
+        particles_total(0)
     {
         this->particles = new double[n_points]();
         this->pions = new double[n_points]();
@@ -42,47 +41,51 @@ struct Simulation {
         this->photons = new double[n_points]();
         this->muons = new double[n_points]();
         this->neutrinos = new double[n_points]();
+        this->energy = new eV_t[n_points]();
 
         this->c_us = 299.792458;
     }
 
+    // distribute the quantity <total> among <count> buckets
+    double* distribute(double total, size_t count, size_t gen) {
+        const size_t c_max = 20;
+        const size_t g_max = 50;
 
-    // double* random_distr(double total, size_t count) {
-    //     // allocate this once
-    //     const size_t c_max = 20;
-    //     static double random_distr[c_max];
-    //     if (count > c_max) {
-    //         cerr << "random_distr count > c_max" << endl;
-    //         exit(1);
-    //     }
-    //     for (int i = 0; i < (count - 1); i++) {
-    //         random_distr[i] = duni(0., total)(generator);
-    //     }
-    //     random_distr[count-1] = total;
-    //     std::sort(random_distr, random_distr + count);
-    //     for (int i = count - 1; i > 0; i--) {
-    //         random_distr[i] -= random_distr[i - 1];
-    //     }
-    //     // yes, this is horrible
-    //     return random_distr;
-    // }
+        // allocate this once
+        static double random_distr[g_max][c_max];
 
-    void count_start () {
+        if (count > c_max) {
+            cerr << "random_distr count > c_max" << endl;
+            exit(1);
+        }
+        if (gen > g_max) {
+            cerr << "random_distr gen > g_max" << endl;
+            exit(1);
+        }
+
+        double sum = 0;
+        for (int i = 0; i < count; i++) {
+            double r = dexp(1.)(generator);
+            random_distr[gen][i] = r;
+            sum += r;
+        }
+        for (int i = 0; i < count; i++) {
+            random_distr[gen][i] /= sum;
+        }
+        // yes, this is horrible
+        return random_distr[gen];
+    }
+
+    // count total number of particles processed
+    void count_start (size_t gen) {
         particles_total++;
-        particles_active++;
         if (particles_total % 100000 == 0) {
-            cerr << setw(12) <<  particles_total << setw(4) << particles_active << endl;
+            cerr << setw(12) <<  particles_total << setw(4) << gen << endl;
         }
     }
 
-    void count_end() {
-        particles_active--;
-    }
-
     // register a particle in an array
-    void trace (double *target, meters_t start, meters_t end) {
-        // cerr << "trace" << start << ' ' << end << endl;
-
+    void trace (double *target, meters_t start, meters_t end, double value) {
         double start_norm = start / depth_step;
         double end_norm = end / depth_step;
 
@@ -108,15 +111,16 @@ struct Simulation {
         }
 
         for (size_t i = i_min; i < i_max; i++) {
-            target[i] += 1.0;
+            target[i] += value;
         }
 
-        target[i_min] -= (start_norm - i_min);
-        target[i_max] += (end_norm - i_max);
+        target[i_min] -= value * (start_norm - i_min);
+        target[i_max] += value * (end_norm - i_max);
     }
 
-    void emag (meters_t d, eV_t e, bool photon) {
-        count_start();
+    // simulate an electron or photon with energy <e> starting at depth d
+    void emag (meters_t d, eV_t e, bool photon, size_t gen) {
+        count_start(gen);
 
         meters_t λ_int = 3.039E+04 * 1e-2;
         if (photon) {
@@ -126,51 +130,51 @@ struct Simulation {
         meters_t interaction = dexp(1./λ_int)(generator);
         meters_t d_end = d + interaction;
 
+        trace(energy, d, d_end, e);
+
         if (photon) {
-            trace(photons, d, d_end);
+            trace(photons, d, d_end, 1);
         } else {
-            trace(electrons, d, d_end);
+            trace(electrons, d, d_end, 1);
         }
 
         if (e > 77e6) {
             if (photon) {
-                emag(d_end, e/2, false);
-                emag(d_end, e/2, false);
+                emag(d_end, e/2, false, gen+1);
+                emag(d_end, e/2, false, gen+1);
             } else {
-                emag(d_end, e/2, true);
-                emag(d_end, e/2, false);
+                emag(d_end, e/2, true, gen+1);
+                emag(d_end, e/2, false, gen+1);
             }
         }
-
-        count_end();
     }
 
-    void muon (meters_t d, eV_t e) {
-        count_start();
+    // simulate a muon with energy <e> starting at depth d
+    void muon (meters_t d, eV_t e, size_t gen) {
+        count_start(gen);
 
         us_t τ = 2.2;
 
         meters_t decay = dexp(1./τ)(generator) * c_us * e / 105.6e6;
         meters_t d_end = d + decay;
 
-        trace(muons, d, d_end);
+        trace(energy, d, d_end, e);
+        trace(muons, d, d_end, 1);
 
-        neutrino(d_end, e/2);
-        emag(d_end, e/2, false);
-
-        count_end();
+        neutrino(d_end, e/2, gen+1);
+        emag(d_end, e/2, false, gen+1);
     }
 
-    void neutrino (meters_t d, eV_t e) {
-        count_start();
+    // simulate a neutrino with energy <e> starting at depth d
+    void neutrino (meters_t d, eV_t e, size_t gen) {
+        count_start(gen);
 
-        trace(neutrinos, d, d + depth_step * n_points);
-        
-        count_end();
+        trace(neutrinos, d, d + depth_step * n_points, 1);
     }
 
-    void pion (meters_t d, eV_t e) {
-        count_start();
+    // simulate a pion with energy <e> starting at depth d
+    void pion (meters_t d, eV_t e, size_t gen) {
+        count_start(gen);
 
         us_t τ = 2.6033e-2;
         meters_t λ_int = 7.477E+04 * 1e-2;
@@ -180,36 +184,35 @@ struct Simulation {
 
         meters_t d_end = d + min(decay, interaction);
 
-        trace(pions, d, d_end);
+        trace(energy, d, d_end, e);
+        trace(pions, d, d_end, 1);
 
         if (interaction < decay) {
             size_t N_ch = 10, N_0 = 5, N_tot = N_ch + N_0;
             // produce new charged pions
             for (int i = 0; i < N_ch; i++) {
-                pion(d_end, e / N_tot);
+                pion(d_end, e / N_tot, gen+1);
             }
             // produce 2*N_0 new photons
             for (int i = 0; i < (2*N_0); i++) {
-                emag(d_end, e / N_tot / 2, true);
+                emag(d_end, e / N_tot / 2, true, gen+1);
             }
         } else {
             double r = duni(0., 1.)(generator);
-            neutrino(d_end, e/2);
-            muon(d_end, e/2);
+            neutrino(d_end, e/2, gen+1);
+            muon(d_end, e/2, gen+1);
         }
-
-        count_end();
     };
 };
 
 int main() {
-    size_t n_points = 3000;
+    size_t n_points = 300;
     meters_t depth_total = 20000;
 
     auto s = new Simulation(n_points, depth_total / n_points);
    
     // s->emag(0, 1e15, true);
-    s->pion(0, 1e16);
+    s->pion(0, 1e14, 0);
 
     for (size_t i = 0; i < n_points; i++)  {
         cout << i * s->depth_step << '\t';
@@ -218,7 +221,8 @@ int main() {
         cout << s->electrons[i] << '\t';
         cout << s->photons[i] << '\t';
         cout << s->muons[i] << '\t';
-        cout << s->neutrinos[i];
+        cout << s->neutrinos[i] << '\t';
+        cout << s->energy[i];
         cout << endl;
     }
     cout << endl;
